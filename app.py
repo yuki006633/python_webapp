e8747adac105370bdd369994ff190f13ac011788
yuki006633 <sanuki1870@gmail.com>
2023-10-30 08:44:07 +0900

    入力フォームを追加

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template #追加
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    name = "Hoge"
-    #return name
-    return render_template('hello.html', title='flask test', name=name) #変更
+# getのときの処理
+@app.route('/', methods=['GET'])
+def get():
+	return render_template('hello.html', \
+		title = 'Form Sample(get)', \
+		message = '名前を入力して下さい。')
 
-## おまじない
-if __name__ == "__main__":
-    app.run(debug=True)
+# postのときの処理
+@app.route('/', methods=['POST'])
+def post():
+	form = request.form
+	iv = form.getlist("iv")
+	return render_template('hello.html', \
+		title = 'Form Sample(post)', \
+		iv = iv)
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
