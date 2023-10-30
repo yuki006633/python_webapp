9fb9784c6fa69eea513e58534c3f540aa4cacfd0
yuki006633 <sanuki1870@gmail.com>
2023-10-30 16:07:42 +0900

    孵化個体シミュレータを追加

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import random as rm
 
 app = Flask(__name__)
 
@@ -14,11 +15,20 @@ def get():
 def post():
 	form = request.form
 	iv = form.getlist("iv")
-	return render_template('hello.html', \
-		title = 'Form Sample(post)', \
-		iv = iv)
+	return render_template('hello.html', 
+		title = 'Form Sample(post)',
+		iv = bleed(iv))
 
-
+#孵化個体シミュレータ
+def bleed(iv):
+	choice = [0,1,2,3,4,5]
+	bleed_iv = [None for _ in range(6)]
+	if True:
+		bleed_iv[rm.randint(0,5)] = rm.randint(0,31)
+		for i in range(6):
+			if bleed_iv[i] == None:
+				bleed_iv[i] = iv[i + 6 * rm.randint(0,1)]
+	return bleed_iv
 
 
 if __name__ == '__main__':
